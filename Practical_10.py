5e97c678e157ade8ee9f6bab1e8d309ad3097667
Pasha <nikolaevpasa4@gmail.com>
2023-05-23 19:42:23 +0300

    Final commit v2

--- Practical_10.py
+++ Practical_10.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-# In this work i decided to change the code for chalenge myself
+# In this work i decided to change the code for challenge myself
+#variant №90 
 
-def f(X, A, b, c, d):
+# Minimaze f(X1,X2) = X1 - A * X2 + b * (X1 ** 2) + c * X1 * X2 + d * (X2 ** 2)
+def f(X, A, b, c, d): 
     X1, X2 = X
     return X1 - A * X2 + b * (X1 ** 2) + c * X1 * X2 + d * (X2 ** 2)
 
@@ -16,43 +18,43 @@ def gradient_descent(X_init, A, b, c, d, l=0.01, eps=1e-6, max_iter=1000):
     X = np.array(X_init) 
     gap = np.inf
     iterations = 0
-    gap_history = []
+    gap_h = []
     
+    #Iterative optimization algorithm
     while gap > eps and iterations < max_iter:
         grad = grad_f(X, A, b, c, d)
         X_new = X - l * grad
         gap = np.linalg.norm(X_new - X)
         X = X_new
         iterations += 1
-        gap_history.append(gap)
+        gap_h.append(gap)
     
-    return X, gap_history
+    return X, gap_h
 
 A = -2
 b = 5
 c = -2
 d = 5
-l = 0.01
-eps = 1e-6
+l = 0.01 #Default Lambda
+eps = 1e-8
 max_iter = 1000
 
-X_starts = [(10, 5), (-7, 1), (8, -9)] 
+start_point = [(9, 6), (80, 160), (200, 300)] #Starting points
 
 optimized_points = []
 gap_histories = []
-
-for X_start in X_starts:
-    X_optimized, gap_history = gradient_descent(X_start, A, b, c, d, l, eps, max_iter)
+for X_start in start_point:
+    X_optimized, gap_h = gradient_descent(X_start, A, b, c, d, l, eps, max_iter)
     optimized_points.append(X_optimized)
-    gap_histories.append(gap_history)
+    gap_histories.append(gap_h)
 
-X1_range = np.linspace(-10, 10, 100)
-X2_range = np.linspace(-10, 10, 100)
+X1_range = np.linspace(-10, 10)#Returns num evenly spaced samples, calculated over the interval [start, stop].
+X2_range = np.linspace(-10, 10)#Returns num evenly spaced samples, calculated over the interval [start stop].
 X1_mesh, X2_mesh = np.meshgrid(X1_range, X2_range)
 Z = f([X1_mesh, X2_mesh], A, b, c, d)
 
-fig = plt.figure(figsize=(10, 8))
-ax = fig.add_subplot(111, projection='3d')
+fig = plt.figure(figsize=(10, 8)) #the chart window dimensions
+ax = fig.add_subplot(111, projection='3d') #IDK is it correct but i did like this and (https://jakevdp.github.io/PythonDataScienceHandbook/04.12-three-dimensional-plotting.html) Information from this page.
 ax.plot_surface(X1_mesh, X2_mesh, Z, cmap='viridis')
 ax.set_xlabel('X1')
 ax.set_ylabel('X2')
@@ -65,24 +67,25 @@ ax.legend()
 
 plt.show()
 
-plt.figure(figsize=(10, 6))
-for i, gap_history in enumerate(gap_histories):
-    plt.plot(range(len(gap_history)), gap_history, label=f'Starting Point {i+1}')
+plt.figure(figsize=(10, 6)) #the chart window dimensions
+for i, gap_h in enumerate(gap_histories):
+    plt.plot(range(len(gap_h)), gap_h, label=f'Starting Point {i+1}')
 plt.xlabel('Iteration')
 plt.ylabel('Optimality Gap')
 plt.title('Optimality Gap vs. Iteration')
 plt.legend()
 plt.show()
 
-lambdas = [0.001, 0.01, 0.1, 1]
+#Draw chart lambda vs Numbers of iteration
+lambdas = [0.01, 0.05 ,0.2 ,0.3, 0.4 ,0.6 ,0.8 , 1] # Array with different values to lambda
 num_iterations = []
 
 for l in lambdas:
-    _, gap_history = gradient_descent(X_starts[0], A, b, c, d, l, eps, max_iter)
-    num_iterations.append(len(gap_history))
+    _, gap_h = gradient_descent(start_point[0], A, b, c, d, l, eps, max_iter)
+    num_iterations.append(len(gap_h))
 
-plt.figure(figsize=(10, 6))
-plt.plot(lambdas, num_iterations, marker='o')
+plt.figure(figsize=(10, 6)) #the chart window dimensions
+plt.plot(lambdas, num_iterations, marker='o') # Numbers of iteration it is y and lambda it is x
 plt.xlabel('Lambda')
 plt.ylabel('Number of Iterations')
 plt.title('Lambda vs. Number of Iterations')
